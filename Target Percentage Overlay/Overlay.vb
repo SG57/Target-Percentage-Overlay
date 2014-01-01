a8e2a6657cba1cb6e175e0c32016bdab574f698d
Cord Rehn <jordansg57@gmail.com>
2013-12-31 16:59:50 -0800

    Huge upgrade!  No longer relies on screen-scraping for percentages, it now pulls the percentage directly from the FFXIV process itself. This makes it incredibly easy to use, more accurate, more reliable and faster.

--- Target Percentage Overlay/Overlay.vb
+++ Target Percentage Overlay/Overlay.vb
@@ -5,220 +5,153 @@ Public Class Overlay
 
 
 
-	' init
-	Private Sub Overlay_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-		MsgBox("A simple overlay that displays the percentage of a static, fill-able bar." & vbCrLf & vbCrLf & "~~Note: the overlay will not overlay properly on a fullscreen application.  If you have 2 monitors, however, you could always put the overlay on the other monitor." & vbCrLf & vbCrLf & "jordansg57@gmail.com - (C) 2013 Cord Rehn", MsgBoxStyle.Information, "Target Percentage Overlay v1.0")
+    ' Handles the process handle grabbing shit
+    Private Declare Function OpenProcess Lib "kernel32.dll" (ByVal dwDesiredAcess As UInt32, ByVal bInheritHandle As Boolean, ByVal dwProcessId As Int32) As IntPtr
+    Private Declare Function ReadProcessMemory Lib "kernel32" (ByVal hProcess As IntPtr, ByVal lpBaseAddress As IntPtr, ByVal lpBuffer() As Byte, ByVal iSize As Integer, ByRef lpNumberOfBytesRead As Integer) As Boolean
+    Private Declare Function CloseHandle Lib "kernel32.dll" (ByVal hObject As IntPtr) As Boolean
+    Private Const FFXIV_PROCESS As String = "ffxiv"
+    Private Const PROCESS_VM_READ As UInteger = 16
+    Private Const PROCESS_QUERY_INFORMATION As UInteger = 1024
+    Private ffxiv_proc As Process
+    Private ffxiv_proc_hdl As IntPtr = IntPtr.Zero
+    Private Function attachToProcess() As Boolean
+        If ffxiv_proc_hdl <> IntPtr.Zero Then Return True ' attached already
 
-		Me.Left = My.Settings.win_x
-		Me.Top = My.Settings.win_y
+        Dim procs As Process() = Process.GetProcessesByName(FFXIV_PROCESS)
+        If procs.Length > 0 Then
+            Me.lblOverlay.Text = "Attaching..."
+            ffxiv_proc = procs(0)
+            ffxiv_proc_hdl = OpenProcess(PROCESS_VM_READ Or PROCESS_QUERY_INFORMATION, False, ffxiv_proc.Id)
+        End If
 
-		Me.Timer1.Interval = My.Settings.refresh
-
-		Me.lblOverlay.Font = My.Settings.font
-		Me.lblOverlay.ForeColor = My.Settings.font_color
-
-		If My.Settings.x1 = -1 Then
-			Me.lblOverlay.Text = "Setup Required"
-		Else
-			Me.lblOverlay.Text = "Not Started"
-		End If
-	End Sub
-
-
-
-	' This all handles click-drag functionality of the overlay
-	Dim drag As Boolean
-	Dim mousex As Integer
-	Dim mousey As Integer
-
-	Private Sub Form1_MouseDown(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles lblOverlay.MouseDown
-		drag = True	'Sets the variable drag to true.
-		mousex = Windows.Forms.Cursor.Position.X - Me.Left 'Sets variable mousex
-		mousey = Windows.Forms.Cursor.Position.Y - Me.Top	'Sets variable mousey
-	End Sub
-
-	Private Sub Form1_MouseMove(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles lblOverlay.MouseMove
-		If drag Then
-			Me.Top = Windows.Forms.Cursor.Position.Y - mousey
-			Me.Left = Windows.Forms.Cursor.Position.X - mousex
-			My.Settings.win_x = Me.Left
-			My.Settings.win_y = Me.Top
-			My.Settings.Save()
-		End If
-	End Sub
-
-	Private Sub Form1_MouseUp(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles lblOverlay.MouseUp
-		drag = False
-	End Sub
+        Return ffxiv_proc_hdl <> IntPtr.Zero
+    End Function
 
 
 
-	' change font color menu option
-	Private Sub ToolStripMenuItem1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ToolStripMenuItem1.Click
-		Dim cDialog As New ColorDialog()
-		cDialog.Color = Me.lblOverlay.ForeColor
-
-		If cDialog.ShowDialog() = DialogResult.OK Then
-			Me.lblOverlay.ForeColor = cDialog.Color
-			My.Settings.font_color = Me.lblOverlay.ForeColor
-			My.Settings.Save()
-		End If
-	End Sub
-
-
-
-	' change font menu option
-	Private Sub ToolStripMenuItem2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ToolStripMenuItem2.Click
-		FontDialog1.Font = Me.lblOverlay.Font
-
-		If FontDialog1.ShowDialog <> Windows.Forms.DialogResult.Cancel Then
-			Me.lblOverlay.Font = FontDialog1.Font
-			My.Settings.font = Me.lblOverlay.Font
-			My.Settings.Save()
-		End If
-	End Sub
+    ' Magic that will get the percentage, rounded to the hundredth-place, of the current target in ffxiv's memory
+    Private Const OFFSET1 = &H20
+    Private Const OFFSET2 = &H10
+    Private Const OFFSET3 = &H24
+    Private Const OFFSET4 = &H20
+    Private Const OFFSET5 = 8
+    Private Const TARGET_OFFSET = &HF8BBE0
+    Public Function ReadInt32(ByVal addr As IntPtr) As Int32
+        Dim _dataBytes(3) As Byte
+        ReadProcessMemory(ffxiv_proc_hdl, addr, _dataBytes, 4, vbNull)
+        Return BitConverter.ToInt32(_dataBytes, 0)
+    End Function
+    Private Function getTargetHealthPercentage() As Double
+        Dim val As Int32 = ReadInt32(ReadInt32(ReadInt32(ReadInt32(ReadInt32(ReadInt32(ffxiv_proc.MainModule.BaseAddress + TARGET_OFFSET) + OFFSET1) + OFFSET2) + OFFSET3) + OFFSET4) + OFFSET5)
+        Return Math.Round(val * 0.01, 2)
+    End Function
 
 
 
-	' close menu option
-	Private Sub CloseToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CloseToolStripMenuItem.Click
-		Me.Timer1.Stop()
-		Me.Close()
-	End Sub
+    ' init
+    Private Sub Overlay_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        MsgBox("An overlay that displays the percentage health of the current target in FFXIV:ARR." & vbCrLf & vbCrLf & "~~Note: the overlay will not overlay in actual fullscreen mode.  If you have 2 monitors, however, you could always put the overlay on the other monitor." & vbCrLf & vbCrLf & "jordansg57@gmail.com - (C) 2013 Cord Rehn", MsgBoxStyle.Information, "Target Percentage Overlay v2.0")
+
+        Me.Left = My.Settings.win_x
+        Me.Top = My.Settings.win_y
+
+        Me.Timer1.Interval = My.Settings.refresh
+
+        Me.lblOverlay.Font = My.Settings.font
+        Me.lblOverlay.ForeColor = My.Settings.font_color
+    End Sub
 
 
 
-	' start/stop menu option
-	Private Sub menuItemStartStop_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles menuItemStartStop.Click
-		If menuItemStartStop.Text = "Start" Then
-			If My.Settings.x1 < 0 Or My.Settings.x2 < 0 Or My.Settings.y1 < 0 Then	' first run / invalid stuff
-				MsgBox("You must first specify the health bar dimensions.", MsgBoxStyle.Exclamation, "Da fauk... thas poolsheet!!")
-				Settings.Show()
-			Else
-				startRunning()
-			End If
-		Else
-			stopRunning()
-		End If
-	End Sub
+    ' This all handles click-drag functionality of the overlay
+    Dim drag As Boolean
+    Dim mousex As Integer
+    Dim mousey As Integer
 
-	Private Sub startRunning()
-		calcDimensions()
+    Private Sub Form1_MouseDown(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles lblOverlay.MouseDown
+        drag = True 'Sets the variable drag to true.
+        mousex = Windows.Forms.Cursor.Position.X - Me.Left 'Sets variable mousex
+        mousey = Windows.Forms.Cursor.Position.Y - Me.Top   'Sets variable mousey
+    End Sub
 
-		Me.Timer1.Start()
-		menuItemStartStop.Text = "Stop"
-		lblOverlay.Text = "--%"
-	End Sub
+    Private Sub Form1_MouseMove(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles lblOverlay.MouseMove
+        If drag Then
+            Me.Top = Windows.Forms.Cursor.Position.Y - mousey
+            Me.Left = Windows.Forms.Cursor.Position.X - mousex
+            My.Settings.win_x = Me.Left
+            My.Settings.win_y = Me.Top
+            My.Settings.Save()
+        End If
+    End Sub
 
-	Private Sub stopRunning()
-		Me.Timer1.Stop()
-		menuItemStartStop.Text = "Start"
-		lblOverlay.Text = "Not Started"
-	End Sub
+    Private Sub Form1_MouseUp(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles lblOverlay.MouseUp
+        drag = False
+    End Sub
 
 
 
-	' this figures out how much percentage of the bar is filled and updates the overlay accordingly
-	Private line_bmp As Bitmap
-	Private bar_width As Integer
-	Private color_intensity As Byte
-	Private bar_rect As Rectangle
+    ' change font color menu option
+    Private Sub ToolStripMenuItem1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ToolStripMenuItem1.Click
+        Dim cDialog As New ColorDialog()
+        cDialog.Color = Me.lblOverlay.ForeColor
 
-	Private Sub calcDimensions()
-		color_intensity = My.Settings.intensity
-		bar_width = My.Settings.x2 - My.Settings.x1
-		bar_rect = New Rectangle(0, 0, bar_width, 1)
-
-		If Not IsNothing(line_bmp) Then line_bmp.Dispose()
-
-		line_bmp = New Bitmap(bar_width, 1, Imaging.PixelFormat.Format24bppRgb)	' no alpha
-	End Sub
-
-	' given bitmap data of a bar, will return the percentage filled the bar is
-	Private Function binarySearchFilledPercentage(ByRef bmd As BitmapData) As Integer
-		Dim low As Integer = 0
-		Dim high As Integer = bmd.Width - 1
-		Dim x As Integer
-
-		Do While low < high
-			x = (low + high) / 2
-
-			Dim offset As Integer = x * 3	' 24 bpp pixel / 8-bits per byte = 3 byte intervals
-			Dim red As Byte = Marshal.ReadByte(bmd.Scan0, offset + 2)
-			Dim green As Byte = Marshal.ReadByte(bmd.Scan0, offset + 1)
-			Dim blue As Byte = Marshal.ReadByte(bmd.Scan0, offset)
-
-			If red < color_intensity And green < color_intensity And blue < color_intensity Then	' possibly do 3 color intensities instead, one for each channel
-				high = x - 1
-			Else
-				low = x + 1
-			End If
-		Loop
-
-		Return Math.Round(low / (bar_width - 1) * 100)
-	End Function
-
-	Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
-		CaptureArea(line_bmp, My.Settings.x1, My.Settings.y1)
-
-		' get bitmap's underlying data
-		Dim bmd As BitmapData = line_bmp.LockBits(bar_rect, System.Drawing.Imaging.ImageLockMode.ReadOnly, line_bmp.PixelFormat)
-
-		Me.lblOverlay.Text = binarySearchFilledPercentage(bmd)
-
-		line_bmp.UnlockBits(bmd)
-
-		If Me.lblOverlay.Text = "0" Then Me.lblOverlay.Text = "--"
-
-		Me.lblOverlay.Text += "%"
-	End Sub
+        If cDialog.ShowDialog() = DialogResult.OK Then
+            Me.lblOverlay.ForeColor = cDialog.Color
+            My.Settings.font_color = Me.lblOverlay.ForeColor
+            My.Settings.Save()
+        End If
+    End Sub
 
 
 
-	' Settings menu option
-	Private Sub ToolStripMenuItem3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ToolStripMenuItem3.Click
-		stopRunning()
-		Settings.Show()
-	End Sub
+    ' change font menu option
+    Private Sub ToolStripMenuItem2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ToolStripMenuItem2.Click
+        FontDialog1.Font = Me.lblOverlay.Font
+
+        If FontDialog1.ShowDialog <> Windows.Forms.DialogResult.Cancel Then
+            Me.lblOverlay.Font = FontDialog1.Font
+            My.Settings.font = Me.lblOverlay.Font
+            My.Settings.Save()
+        End If
+    End Sub
 
 
 
-	' How to use it
-	Private Sub ToolStripMenuItem4_Click(sender As System.Object, e As System.EventArgs) Handles ToolStripMenuItem4.Click
-		MsgBox("1. Drag the text overlay wherever you want." & vbCrLf & "2. Right-click the overlay -> Settings" & vbCrLf & "3. Click the 'Find' button next to the 0% location fields." & vbCrLf & "4. Roughly, hover over the pixel that marks 0% of the bar filled and press any key.  Triple-check the color of the pop-up dialog matches the bar to make sure you're over the bar!!" & vbCrLf & "5. Repeat step 4, but for the pixel that marks the 100% filled bar." & vbCrLf & vbCrLf & "That's it!  Save, and Right-click -> Start the overlay." & vbCrLf & vbCrLf & "If you're experiencing some glitchiness, go back and tweak the coordinates slightly and the color intensity parameter." & vbCrLf & vbCrLf & "jordansg57@gmail.com - (C) 2013 Cord Rehn", MsgBoxStyle.Information, "How To Use It")
-	End Sub
+    ' close menu option
+    Private Sub CloseToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CloseToolStripMenuItem.Click
+        Me.Timer1.Stop()
+        Me.Close()
+    End Sub
 
 
 
-	' donate button
-	Private Sub ToolStripMenuItem5_Click(sender As System.Object, e As System.EventArgs) Handles ToolStripMenuItem5.Click
-		System.Diagnostics.Process.Start("https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=JordanSg57%40gmail%2ecom&lc=US&item_name=Freelance%20Developer&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted")
-	End Sub
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        If Not attachToProcess() Then
+            Me.lblOverlay.Text = "FFXIV Not Running..."
+
+        Else ' attached to ffxiv process, scrape memory for percentage
+            Me.lblOverlay.Text = getTargetHealthPercentage() + "%"
+        End If
+    End Sub
 
 
 
-	' required modules for screen capture
-	Private Declare Auto Function BitBlt Lib "gdi32.dll" (ByVal hdcDest As IntPtr, ByVal nXDest As Integer, ByVal nYDest As Integer, ByVal nWidth As Integer, ByVal nHeight As Integer, ByVal hdcSrc As IntPtr, ByVal nXSrc As Integer, ByVal nYSrc As Integer, ByVal dwRop As Int32) As Boolean
-	Private Declare Auto Function GetDC Lib "user32.dll" (ByVal hWnd As IntPtr) As IntPtr
-	Private Declare Auto Function ReleaseDC Lib "user32.dll" (ByVal hWnd As IntPtr, ByVal hDC As IntPtr) As IntPtr
-
-	' fill 'line_bmp' with 'area' of the screen.  expects line_bmp to be setup prior. Needs manual disposal once finished with!
-	Public Sub CaptureArea(ByRef bmp As Bitmap, ByRef x As Integer, ByRef y As Integer)
-		Using g As Graphics = Graphics.FromImage(bmp)
-			Dim srcDC As IntPtr = GetDC(IntPtr.Zero)
-			Dim destDC As IntPtr = g.GetHdc()
-
-			BitBlt(destDC, 0, 0, bmp.Width, bmp.Height, srcDC, x, y, 13369376) 'SRCCOPY = 13369376
-
-			g.ReleaseHdc(destDC)
-			ReleaseDC(IntPtr.Zero, srcDC)
-		End Using
-	End Sub
+    ' Settings menu option
+    Private Sub ToolStripMenuItem3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ToolStripMenuItem3.Click
+        Settings.Show()
+    End Sub
 
 
 
-	' on closing
-	Private Sub onClose(ByVal sender As Object, ByVal e As System.EventArgs) Handles MyBase.FormClosing
-		If Not IsNothing(line_bmp) Then line_bmp.Dispose()
-	End Sub
+    ' donate button
+    Private Sub ToolStripMenuItem5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ToolStripMenuItem5.Click
+        System.Diagnostics.Process.Start("https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=JordanSg57%40gmail%2ecom&lc=US&item_name=Freelance%20Developer&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted")
+    End Sub
+
+
+
+    ' close
+    Private Sub Overlay_FormClosed() Handles MyBase.FormClosed
+        If ffxiv_proc_hdl <> IntPtr.Zero Then CloseHandle(ffxiv_proc_hdl)
+    End Sub
 End Class
--- Target Percentage Overlay/Settings.vb
+++ Target Percentage Overlay/Settings.vb
@@ -1,54 +1,17 @@
 ﻿Public Class Settings
 
-
-
 	' init from settings prefs
 	Private Sub Settings_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-		box0_x.Text = My.Settings.x1
-		box0_y.Text = My.Settings.y1
-		box100_x.Text = My.Settings.x2
 		boxRefreshTimer.Text = My.Settings.refresh
-		boxHealthIntensity.Text = My.Settings.intensity
 	End Sub
-
-
 
 	' save click
 	Private Sub btnSave_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnSave.Click
-		' basic boundary checks. don't care.
-		If Integer.Parse(box0_x.Text) < 0 Or Integer.Parse(box100_x.Text) < 0 Or Integer.Parse(box0_y.Text) < 0 Then
-			MsgBox("You must set valid coordinates!", MsgBoxStyle.Exclamation, "Grr...")
-			Exit Sub
-		End If
-
-		My.Settings.x1 = box0_x.Text
-		My.Settings.y1 = box0_y.Text
-		My.Settings.x2 = box100_x.Text
-
 		My.Settings.refresh = boxRefreshTimer.Text
 		Overlay.Timer1.Interval = boxRefreshTimer.Text
 
-		My.Settings.intensity = boxHealthIntensity.Text
+        My.Settings.Save()
 
-		My.Settings.Save()
-
-		Overlay.lblOverlay.Text = "Not Started"
 		Me.Close()
-	End Sub
-
-
-
-	' find 0% loc btn click
-	Private Sub btnFind0_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnFind0.Click
-		Find.Show()
-		Find.find_0 = True
-	End Sub
-
-
-
-	' find 100% loc btn click
-	Private Sub btnFind100_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnFind100.Click
-		Find.Show()
-		Find.find_0 = False
 	End Sub
 End Class
